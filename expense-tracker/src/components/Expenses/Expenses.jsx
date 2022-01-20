d370cbb2ec9854b11bc9c8f2f32b382dd36280d9
Aditya <akaditya394@gmail.com>
2022-01-20 19:05:29 +0530

    filter component added

--- expense-tracker/src/components/Expenses/Expenses.jsx
+++ expense-tracker/src/components/Expenses/Expenses.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import './Expenses.css';
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+
+  const [filteredYear, setFilteredYear] = useState('2020');
+  
+  const filterChangeHandler = (selectedYear) =>{
+    setFilteredYear(selectedYear);
+  }
+
+  
   return (
+
+    <div>
+    
     <Card className="expenses">
+    <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
       <ExpenseItem
         title={props.items[0].title}
         date={props.items[0].date}
@@ -27,6 +40,7 @@ const Expenses = (props) => {
         amount={props.items[3].amount}
       />
     </Card>
+    </div>
   );
 };
 
